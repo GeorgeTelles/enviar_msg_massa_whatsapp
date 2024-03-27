1ff4db2e5ccdbe7bba23b9efb7440e777a7c9409
George Telles <george_1@hotmail.com.br>
2024-03-27 19:19:52 -0300

    Add files via upload

---
+++ group_sender.py
@@ -0,0 +1,15 @@
+import pywhatkit as kt
+import pyautogui as pg
+import time
+
+
+
+kt.sendwhats_image("L2JEJC5S51oCh1E30oQhFp","C:/Users/Georg/Desktop/Chatbot/Group Sender/166.JPG","Estoy cansada \n fdasfdasfd",10)
+time.sleep(5)  # Espera por 5 segundos
+pg.press("enter")
+time.sleep(5)  # Espera por 5 segundos
+
+kt.sendwhats_image("EWONnbZtFaB18TjxBL0EwW","C:/Users/Georg/Desktop/Chatbot/Group Sender/meme.png","Estoy cansada",10)
+time.sleep(5)  # Espera por 5 segundos
+pg.press("enter")
+time.sleep(5)  # Espera por 5 segundos
